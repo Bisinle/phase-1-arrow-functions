93ad914edffb65235d852f256fcd4efc0dcc0433
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-06-13 13:21:21 +0300

    Arrow function lab DONE

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = ()=>{    
+    return 2000/100
+}
+console.log( divide());
+
+
+/********************************/
+const square = (number)=>{
+    return number*number
+
+}
+
+const add = (a,b)=>{
+    return a+b
+
+}
